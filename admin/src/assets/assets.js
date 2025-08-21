7649a978929a7aa834c66436d7100fcca7ab2b0e
Altamash Rezaan <156195153+altamash08@users.noreply.github.com>
2025-08-21 15:42:17 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,7 +5,7 @@ import order_icon from './order_icon.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
-export const url = 'http://localhost:4000'
+export const url = 'https://food-delivery-website-backend-s65o.onrender.com'
 export const currency = '₹'
 
 export const assets ={
