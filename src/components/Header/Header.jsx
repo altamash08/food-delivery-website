7e51c84b9641bdbbdadde570141c4ccfcfba6791
Altamash Rezaan <156195153+altamash08@users.noreply.github.com>
2026-02-02 20:14:07 +0530

    Replace old repo with new React + Vite project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Header.css';
+import { assets } from '../../assets/assets'; // Import the assets object
+
+const Header = () => {
+    return (
+        <div
+            className='header'
+            style={{
+                backgroundImage: `url(${assets.dashboard_img})`, // Use the imported dashboard image
+            }}
+        >
+            <div className='header-contents'>
+                <h2>Order your favourite food here</h2>
+                <p>
+                    Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.
+                </p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
