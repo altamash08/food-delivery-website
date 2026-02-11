a05758810e1039da538cdfb4420dcce60ac1b42c
Altamash Rezaan <156195153+altamash08@users.noreply.github.com>
2026-02-11 11:55:20 +0530

    Initial commit with my project

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://altusmanezzu:finalyearproject2025@cluster0.x9t825d.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://altamashrizwan546_db_user:Altamash08@cluster0.ojfhioo.mongodb.net/food-del').then(()=>console.log("DB Connected"));
    
 }
 
